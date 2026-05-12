0bc6dcbcd711737470dd04c9c16c2f1b5dd36b9e
Junho Hwang <wnsgh224@naver.com>
2026-05-12 11:24:14 +0900

    feat(SimpleGraph): add cycle deletion edge count lemma

--- Mathlib/Combinatorics/SimpleGraph/Connectivity/DeleteEdges.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity/DeleteEdges.lean
@@ -126,6 +126,17 @@ theorem exists_cycle_of_mem_edgeSet_of_forall_even_degree [Finite V]
   | h u v =>
       exact G.exists_cycle_of_adj_of_forall_even_degree (by simpa using he) heven
 
+/-- Deleting the edges of a cycle decreases the number of edges. -/
+theorem card_edgeFinset_deleteEdges_cycle_lt [DecidableEq V] [Fintype G.edgeSet]
+    {u : V} {p : G.Walk u u} [Fintype (G.deleteEdges p.edges.toFinset).edgeSet]
+    (hp : p.IsCycle) :
+    (G.deleteEdges p.edges.toFinset).edgeFinset.card < G.edgeFinset.card := by
+  apply G.card_edgeFinset_deleteEdges_lt p.edges.toFinset
+  rcases hp.edges_toFinset_nonempty with ⟨e, he⟩
+  exact ⟨e, by
+    rw [Finset.mem_inter]
+    exact ⟨p.edges_toFinset_subset_edgeFinset he, he⟩⟩
+
 /-- In a finite graph in which every vertex has even degree, no edge is a bridge. -/
 theorem not_isBridge_of_adj_of_forall_even_degree [Finite V] [∀ x : V, Fintype (G.neighborSet x)]
     (hGuv : G.Adj u v) (heven : ∀ x, Even (G.degree x)) : ¬ G.IsBridge s(u, v) := by
